7f1d6360f07725d3426935da19d244db0601a57c
Slimcent <centpresley@gmail.com>
2023-11-19 09:54:00 +0100

    main project

--- main.py
+++ main.py
@@ -1,8 +1,63 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers, models
+import keras
+from keras import layers, models
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Load the Fashion MNIST dataset
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
+
+# Define the neural network model
+model = models.Sequential()
+model.add(layers.Flatten(input_shape=(28, 28)))  # Flatten layer
+model.add(layers.Dense(128, activation='relu'))  # Dense layer with ReLU activation
+model.add(layers.Dropout(0.5))  # Dropout layer
+model.add(layers.Dense(64, activation='relu'))  # Additional Dense layer
+model.add(layers.Dense(10, activation='softmax'))  # Output layer with softmax activation
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, validation_split=0.2)
+
+# Evaluate the model on the test set
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print(f'Test accuracy: {test_acc * 100:.2f}%')
+
+# Make predictions on the test set
+y_pred = np.argmax(model.predict(X_test), axis=1)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Generate a confusion matrix and classification report
+confusion = confusion_matrix(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print("Confusion Matrix:")
+print(confusion)
+print("Classification Report:")
+print(report)
+
+# Plot the Confusion Matrix
+plt.figure(figsize=(8, 6))
+sns.heatmap(confusion, annot=True, cmap='Blues', fmt='g', xticklabels=np.unique(y_test), yticklabels=np.unique(y_test))
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
+
 
